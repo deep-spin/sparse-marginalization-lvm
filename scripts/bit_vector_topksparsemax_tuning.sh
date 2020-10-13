e6f32c3d3e0708fde5e4342a840bcc48ef485cb3
goncalomcorreia <goncalommac@gmail.com>
2020-10-13 15:11:56 +0100

    add scripts for hyperparameter tuning (bit-vector experiment)

---
+++ scripts/bit_vector_topksparsemax_tuning.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for lr in 5e-5 1e-4 5e-4 1e-3 5e-3
+do
+    python experiments/bit_vector-vae/train.py \
+        --mode topksparse \
+        --lr $lr \
+        --batch_size 64 \
+        --n_epochs 100 \
+        --latent_size 32 \
+        --weight_decay 0.
+done
